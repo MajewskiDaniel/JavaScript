8d828f528d0040f98f345cc2f1129ed3b78dd465
Daniel Majewski <danielmajewski82@gmail.com>
2020-04-30 16:32:25 +0200

    banner added

---
+++ other projects/banner/main.js
@@ -0,0 +1,40 @@
+const slideList = [
+  {
+    img: "images/img1.jpg",
+    text: "First text",
+  },
+  {
+    img: "images/img2.jpg",
+    text: "Second text",
+  },
+  {
+    img: "images/img3.jpg",
+    text: "Third text",
+  },
+];
+
+const image = document.querySelector("img.slider");
+const h1 = document.querySelector("h1.slider");
+const dots = [...document.querySelectorAll(".dots span")];
+
+// Interface
+const time = 5000;
+let active = 0;
+
+// Implenetation
+const changeDot = () => {
+  const activeDot = dots.findIndex((dot) => dot.classList.contains("active"));
+  dots[activeDot].classList.remove("active");
+  dots[active].classList.add("active");
+};
+
+const changeSlide = () => {
+  active++;
+  if (active === slideList.length) {
+    active = 0;
+  }
+  image.src = slideList[active].img;
+  h1.textContent = slideList[active].text;
+  changeDot();
+};
+setInterval(changeSlide, time);
