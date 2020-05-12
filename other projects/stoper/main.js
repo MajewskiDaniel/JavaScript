160a8f28d280d75d47fb110636a076b861b88ac6
Daniel Majewski <danielmajewski82@gmail.com>
2020-05-12 15:02:36 +0200

    one function fixed

--- other projects/stoper/main.js
+++ other projects/stoper/main.js
@@ -6,14 +6,6 @@ let counter = 0;
 let start;
 
 const startStopTime = () => {
-  const countingFunction = () => {
-    counter++;
-    if (counter % 100 === 0) {
-      display.textContent = `${counter / 100}.00`;
-    } else if (counter % 10 === 0) {
-      display.textContent = `${counter / 100}0`;
-    } else display.textContent = `${counter / 100}`;
-  };
   if (mainBtn.textContent === "Start") {
     start = setInterval(countingFunction, 10);
     mainBtn.textContent = "Stop";
@@ -21,6 +13,15 @@ const startStopTime = () => {
     clearInterval(start);
     mainBtn.textContent = "Start";
   }
+};
+
+const countingFunction = () => {
+  counter++;
+  if (counter % 100 === 0) {
+    display.textContent = `${counter / 100}.00`;
+  } else if (counter % 10 === 0) {
+    display.textContent = `${counter / 100}0`;
+  } else display.textContent = `${counter / 100}`;
 };
 
 const resetTime = () => {
